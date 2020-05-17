388418c99b66cefbbffc73217726af3515b7c5fc
burtsev <Cegthjhnbrjybubg24>
2020-05-17 23:54:41 +0300

    String send works, string recieve and separation Topic and data works too.

--- ZMQ_read.py
+++ ZMQ_read.py
@@ -3,20 +3,35 @@ import zmq
 
 port = "5556"
 if len(sys.argv) > 1:
-    port = sys.argv[1]
+    port =  sys.argv[1]
     int(port)
 
 if len(sys.argv) > 2:
-    port1 = sys.argv[2]
+    port1 =  sys.argv[2]
     int(port1)
 
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-print
-"Collecting updates from weather server..."
-socket.connect("tcp://localhost:%s" % port)
+print( "Collecting updates from weather server..." )
+socket.connect ("tcp://localhost:%s" % port)
 
 if len(sys.argv) > 2:
-    socket.connect("tcp://localhost:%s" % port1)
+    socket.connect ("tcp://localhost:%s" % port1)
+
+# Subscribe to zipcode, default is NYC, 10001
+topicfilter = b"10001"
+socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
+
+# Process 5 updates
+total_value = 0
+for update_nbr in range (5):
+    string = str(socket.recv())
+    print(string)
+    topic, messagedata = string.split('_')
+    #total_value += int(messagedata)
+    print( topic, messagedata )
+
+#print( "Average messagedata value for topic '%s' was %dF" % (topicfilter, total_value / update_nbr) )
+
